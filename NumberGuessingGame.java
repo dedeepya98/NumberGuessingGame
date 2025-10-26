cccc4a33d6a216db6b7012724247af62d7041d71
dedeepya89 <dedeepya899@gmail.com>
2025-10-25 21:30:06 -0400

    initial commit

---
+++ NumberGuessingGame.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+public class NumberGuessingGame {
+
+    public static void main(String[] args){
+        //instantiating scanner class
+        Scanner scanner = new Scanner(System.in);
+        //instantiating random class
+        Random rand = new Random();
+        
+        
+        //asking the user what number they want to guess till
+        System.out.println("Up to what number do you want to guess? ");
+        
+        //getting the response and generating the random number between 1 and whatever they picked
+        int response = scanner.nextInt();
+        int randomNum = rand.nextInt(response) + 1;
+     
+        //output that says enter your guess from 1 and whatever number player picked
+        System.out.println("Enter your guess (1-" + response + "): ");
+
+        //instantiating the amount of time we go through the loop
+        int tryCount = 0; 
+        
+        
+        
+        //checking if guess is correct
+        while(true){
+            
+            //read the input
+            int playerGuess = scanner.nextInt();
+            tryCount ++;
+            if (playerGuess == randomNum){
+                System.out.println("Congratualtions! You guessed correctly! You guessed " + tryCount + " times.");
+                break;
+            }
+            else if (playerGuess < randomNum){
+                System.out.println("Guess a bigger number");
+            }
+            else{
+                System.out.println("Guess a smaller numner");
+            }
+
+            System.out.println("Renter your guess (1-100): ");
+
+        }
+
+        scanner.close();
+    }
+} 
---
+++ README.md
@@ -0,0 +1,8 @@
+README.md
+    This is a number guessing game.
+    Rules for this game:
+        -System creates a random number
+        -Player enters a guess for what the number can be
+        -System will tell you if the guess is higher or lower
+        -Player can try until they guess correctly
+        -If player guesses correctly, they will get a "Congratualtions! You guessed correctly!" message
